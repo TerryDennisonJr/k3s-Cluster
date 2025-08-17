e9d4e4a9a05f55ef875e8590fbe47e5f807614a8
TerryDennisonJr <terrysc85@gmail.com>
2025-08-16 19:46:57 -0600

    Hlem refactor

--- python/raspi_weather_app/app_teardown.sh
+++ python/raspi_weather_app/app_teardown.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-kubectl delete secret mysql-secret
-helm delete weather-app helm/pi-weather-chart
+helm delete weather-app
+#Agent helm file
 rm -r ~/Desktop/k3s-homelab/k3s-Cluster/datadog_observability/datadog/datadog.yaml
 cp -r ~/Desktop/k3s-homelab/k3s-Cluster/datadog_observability/datadog/template/template_datadog.yaml ~/Desktop/k3s-homelab/k3s-Cluster/datadog_observability/datadog/datadog.yaml
+#Sensehat app file
+# rm -r ~/Desktop/k3s-homelab/k3s-Cluster/python/raspi_weather_app/k8s_weather/sensehat_mysql.py
+# cp -r ~/Desktop/k3s-homelab/k3s-Cluster/python/template_files/sensehat_mysql.py ~/Desktop/k3s-homelab/k3s-Cluster/python/raspi_weather_app/k8s_weather/
+
 helm upgrade datadog-agent -f ~/Desktop/k3s-homelab/k3s-Cluster/datadog_observability/datadog/datadog.yaml  datadog/datadog
